e7db3dad3c03006e629e91a37e5348adff08bebf
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2019-12-12 19:41:26 +0100

    ADd light switch component

---
+++ src/app/light-switch/light-switch.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { IotService } from "../iot.service";
+
+@Component({
+  selector: "app-light-switch",
+  templateUrl: "./light-switch.component.html",
+  styleUrls: ["./light-switch.component.scss"]
+})
+export class LightSwitchComponent {
+  state: Boolean | undefined = undefined;
+  @Input() deviceUuid: string;
+  @Input() variableUuid: string;
+  @Input() name: string;
+
+  constructor(private iot: IotService) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.iot
+        .observe(this.deviceUuid, this.variableUuid)
+        .subscribe((newValue: any) => {
+          console.log("new value", newValue);
+          if (newValue !== undefined) {
+            this.state = newValue.state;
+          } else {
+            this.state = undefined;
+          }
+        });
+    }, 1000);
+  }
+
+  toggle() {
+    const state = !this.state;
+    this.iot.setValue(this.deviceUuid, this.variableUuid, { state });
+  }
+}
