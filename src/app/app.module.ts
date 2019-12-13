504bbd9c84be079ae96d848d780c1aad3087957c
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2019-12-13 19:25:36 +0100

    Progresify app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LightSwitchComponent } from './light-switch/light-switch.component';
-import { ChristmasTreeSwitchComponent } from './christmas-tree-switch/christmas-tree-switch.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LightSwitchComponent } from "./light-switch/light-switch.component";
+import { ChristmasTreeSwitchComponent } from "./christmas-tree-switch/christmas-tree-switch.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -14,9 +16,12 @@ import { ChristmasTreeSwitchComponent } from './christmas-tree-switch/christmas-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
