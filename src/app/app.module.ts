43b45d28273a71bc3f521b540f5f38bbc3cdb38b
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-11-22 15:45:47 +0100

    Remove iot christmass components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LightSwitchComponent } from "./light-switch/light-switch.component";
-import { ChristmasTreeSwitchComponent } from "./christmas-tree-switch/christmas-tree-switch.component";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
-import { StripAnimationComponent } from "./strip-animation/strip-animation.component";
 import { SlickModule } from "ngx-slick";
-import { SensorsComponent } from "./sensors/sensors.component";
-import { MoistureGaugeComponent } from "./sensors/moisture-gauge/moisture-gauge.component";
 import { LoginComponent } from "./login/login.component";
 import { RootComponent } from "./root/root.component";
 
@@ -18,22 +14,18 @@ import { RootComponent } from "./root/root.component";
   declarations: [
     AppComponent,
     LightSwitchComponent,
-    ChristmasTreeSwitchComponent,
-    StripAnimationComponent,
-    SensorsComponent,
-    MoistureGaugeComponent,
     LoginComponent,
-    RootComponent
+    RootComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SlickModule.forRoot(),
     ServiceWorkerModule.register("ngsw-worker.js", {
-      enabled: environment.production
-    })
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [RootComponent]
+  bootstrap: [RootComponent],
 })
 export class AppModule {}
--- src/app/christmas-tree-switch/christmas-tree-switch.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from "@angular/core";
-import { LightSwitchComponent } from "../light-switch/light-switch.component";
-
-@Component({
-  selector: "app-christmas-tree-switch",
-  templateUrl: "./christmas-tree-switch.component.html",
-  styleUrls: ["./christmas-tree-switch.component.scss"]
-})
-export class ChristmasTreeSwitchComponent extends LightSwitchComponent {}
