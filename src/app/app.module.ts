f1746dca088b78f79f44b89a08a6f56e2d5d45be
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-11-23 20:22:30 +0100

    Remove light switch component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LightSwitchComponent } from "./light-switch/light-switch.component";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { SlickModule } from "ngx-slick";
@@ -13,7 +12,6 @@ import { RootComponent } from "./root/root.component";
 @NgModule({
   declarations: [
     AppComponent,
-    LightSwitchComponent,
     LoginComponent,
     RootComponent,
   ],
