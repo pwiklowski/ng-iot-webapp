53b4a4ae11828b24c2a014225f2bf0fe09dabe44
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-12-20 17:20:05 +0100

    Add loading animation

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,6 +1,5 @@
 import { AuthService } from "@auth0/auth0-angular";
 import { Component, OnInit } from "@angular/core";
-import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -8,21 +7,11 @@ import { Router, NavigationEnd } from "@angular/router";
   styleUrls: ["./root.component.scss"],
 })
 export class RootComponent implements OnInit {
-  isLoggedIn = false;
+  isLoggedIn = undefined;
 
-  constructor(router: Router, public auth: AuthService) {
-    auth.isAuthenticated$.subscribe((res) => (this.isLoggedIn = res));
-
-    router.events.subscribe((s: NavigationEnd) => {
-      if (s instanceof NavigationEnd && s.url && s.url.indexOf("login") === -1) {
-        console.log(s.url);
-        // if (auth.isTokenValid()) {
-        //   console.log("token is valid");
-        // } else {
-        //   console.log("new token is needed", router.url);
-        //   router.navigate(["/login"]);
-        // }
-      }
+  constructor(public auth: AuthService) {
+    auth.isAuthenticated$.subscribe((res) => {
+      this.isLoggedIn = res;
     });
   }
 
