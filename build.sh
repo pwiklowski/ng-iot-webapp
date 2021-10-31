5564aefb12deb0ea5684750aa27d8313d103df70
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2021-10-31 14:48:28 +0100

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 VERSION=`cat package.json | jq -r '.version'`
 
-npm install
-npm run build
-
 docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD registry.wiklosoft.com
 docker build -t registry.wiklosoft.com/iot-dash:$VERSION . --platform linux/amd64
 docker push registry.wiklosoft.com/iot-dash:$VERSION
